30d3eca1102125df189b40a2190c765d5f93287d
Kelton Green <greenkelton@protonmail.com>
2021-06-12 23:57:08 -0600

    patch

--- batter/game/handle_collisions_action.py
+++ batter/game/handle_collisions_action.py
@@ -2,21 +2,19 @@ from game.point import Point
 import random
 from game import constants
 from game.action import Action
-from game.constants import MAX_Y
+from game.constants import LEN_PADDLE, MAX_Y, MAX_X
 from time import sleep
 import sys
 
 
 class HandleCollisionsAction(Action):
     """A code template for handling collisions. The responsibility of this class of objects is to update the game state when actors collide.
-    
     Stereotype:
         Controller
     """
 
     def execute(self, cast):
         """Executes the action using the given actors.
-
         Args:
             cast (dict): The game actors {key: tag, value: list}.
         """
@@ -34,7 +32,7 @@ class HandleCollisionsAction(Action):
         ball = cast["ball"][0]  # there's only one
         paddle = cast["paddle"][0]  # there's only one
         leftWall = 0
-        rightWall = constants.MAX_X
+        rightWall = constants.MAX_X - 1
         if (
             ball.get_position().get_x() >= rightWall
             or ball.get_position().get_x() <= leftWall
@@ -54,14 +52,17 @@ class HandleCollisionsAction(Action):
 
     def _ball_ceiling_collision(self, cast):
         """Handles the times when the ball hits the ceiling.
-
         Args:
             cast (dict): The game actors {key: tag, value: list}.
         """
         ball = cast["ball"][0]  # there's only one
-        ceiling = constants.MAX_Y
+        # ceiling = constants.MAX_Y
+        # (AH) ceiling is (0, 0)
+        ceiling = 0
 
-        if ball.get_position().get_y() >= ceiling:
+        # if ball.get_position().get_y() >= ceiling:
+        # (AH) test for ceiling and below.
+        if ball.get_position().get_y() <= ceiling:
             # change direction of ball
             point = ball.get_velocity()
             newVel = Point(point.get_x(), -point.get_y())
@@ -76,12 +77,24 @@ class HandleCollisionsAction(Action):
         paddle = cast["paddle"][0]
         if ball.get_position().equals(paddle.get_position()):
             point = ball.get_velocity()
-            newVel = Point(point.get_x(), point.get_y())
-            ball.set_velocity(newVel)
+            # newVel = Point(point.get_x(), point.get_y())
+            # ball.set_velocity(newVel)
+            # (AH) ball should bounce off paddle.
+
+            # (AH) Case where ball bounced from left at an angle.
+            # (AH) Ball bounce in opposite direction, both horizontal+vertical.
+            if point.get_x() > 0 and point.get_y() > 0:
+                opp_vel = Point(point.get_x(), -point.get_y())
+                ball.set_velocity(opp_vel)
+
+            # (AH) Case where ball bounced from right at an angle.
+            # (AH) Ball bounce in opposite direction, both horizontal+vertical.
+            elif point.get_x() < 0 and point.get_y() > 0:
+                opp_vel = Point(point.get_x(), -point.get_y())
+                ball.set_velocity(opp_vel)
 
     def _ball_brick_collision(self, cast):
         """Handles the times when the ball or the paddle hits either wall.
-
             Args:
                 cast (dict): The game actors {key: tag, value: list}.
         """
@@ -126,14 +139,15 @@ class HandleCollisionsAction(Action):
                 # (AH) then exit loop because loop has changed after pop.
                 bricks.pop(index)
                 break
-                
+
     def _ball_floor_collision(self, cast):
         """Handles the collision of the ball hitting the floor.
-
         Args:
             cast (dict): the game actors {key: tag, value: list}.
         """
         p_ball_y = cast["ball"][0].get_position().get_y()
-        if (p_ball_y > MAX_Y + 2):
+        # if p_ball_y > MAX_Y + 2:
+        # (AH) up to but not including MAX_Y
+        if p_ball_y > MAX_Y:
             sleep(2)
             sys.exit()
